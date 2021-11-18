38c646aa670c867ae45f8ee1606a48dbd414babe
williammongou <williammongou@gmail.com>
2021-11-17 23:13:46 -0600

    fixed bug and added readme document

--- src/Hello.java
+++ src/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("The conflict is solved");
+		System.out.println("A bug is fixed on bug fix branch");
 
 	}
 
